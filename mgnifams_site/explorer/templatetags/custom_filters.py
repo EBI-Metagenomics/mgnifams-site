280f16c0712a97298bbb93bb2a55efb4b6268874
vagkaratzas <vagelaros.gee@gmail.com>
2023-12-22 11:32:16 +0000

    MGYPs, masks and links to PLP

--- mgnifams_site/explorer/templatetags/custom_filters.py
+++ mgnifams_site/explorer/templatetags/custom_filters.py
@@ -3,15 +3,3 @@
 from django import template
 
 register = template.Library()
-
-@register.filter(name='format_id')
-def format_id(value):
-    mgnifam_name = str(value).split('_')[0]
-    return mgnifam_name
-
-@register.filter(name='family_size')
-def get_family_size(value):
-    split_part = str(value).split('_')[1]
-    size = split_part.split('-')[0]
-
-    return size
--- mgnifams_site/explorer/views.py
+++ mgnifams_site/explorer/views.py
@@ -30,9 +30,16 @@ def id_exists(check_id, filepath):
     with open(filepath, 'r') as f:
         return check_id in f.read().splitlines()
 
+def format_protein_name(raw_name):
+    """
+    Formats the protein name by appending zeros in front to make it 12 characters,
+    and then adds 'MGYP' as a prefix.
+    """
+    formatted_name = raw_name.zfill(12)  # Append zeros to make it 12 characters
+    return "MGYP" + formatted_name
+
 def details(request):
     id = request.GET.get('id', None)
-
     base_dir = "../data/"
     id_filepath = os.path.join(base_dir, 'mgnifam_names.txt')
 
@@ -40,22 +47,37 @@ def details(request):
     if not id_exists(id, id_filepath):
         messages.error(request, 'Invalid ID entered. Please check and try again.')
         return redirect('index')
-
     # Construct the path for the cif file
     cif_directory = os.path.join(base_dir, 'cif/')
-    cif_files = glob.glob(os.path.join(cif_directory, id + '*'))
-    
+    cif_files = glob.glob(os.path.join(cif_directory, id + '_*'))
     # Extract only the filename from the first matching cif file
     cif_filename = os.path.basename(cif_files[0]) if cif_files else None
-
     # Check if cif file exists
     if not cif_filename:
         messages.error(request, 'No CIF file found for the given ID.')
         return redirect('index')
 
+    filename_no_ext = cif_filename.split('.')[0]
+    first_split = filename_no_ext.split('-')
+    first_split_first_part = first_split[0]
+    first_split_second_part = first_split[1]
+    family_id = first_split_first_part.split('_')[0]
+    family_size = first_split_first_part.split('_')[1]
+    protein_parts = first_split_second_part.split('_')
+    protein_rep = format_protein_name(protein_parts[0])
+    if (len(protein_parts) == 1):
+        mask = "whole MGYP protein"
+    elif (len(protein_parts) == 3):
+        mask = f"{protein_parts[1]}-{protein_parts[2]}"
+    elif (len(protein_parts) == 5):
+        mask = f"{int(protein_parts[1]) + int(protein_parts[3]) - 1}-{int(protein_parts[1]) + int(protein_parts[4]) - 1}"
+
     return render(request, 'explorer/details.html', {
-        'id': id,
-        'cif_path': cif_filename  # Updated to use just the filename
+        'family_id': family_id,
+        'family_size': family_size,
+        'cif_path': cif_filename,  
+        'protein_rep': protein_rep,
+        'mask': mask
     })
 
 def mgnifam_names(request):
