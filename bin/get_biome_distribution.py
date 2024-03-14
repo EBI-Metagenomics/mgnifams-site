c32b42a65e2ad26db3b846bfddb21e1c464fcc1d
vagkaratzas <vagelaros.gee@gmail.com>
2024-03-14 12:21:25 +0000

    get scripts for biomes and pfams updated to start above given fam id

--- bin/get_biome_distribution.py
+++ bin/get_biome_distribution.py
@@ -48,19 +48,29 @@ def parse_col2(col2):
         return col2.split('_')[0]
     return col2
 
-def query_sequence_explorer_protein(cursor, edge_list_file, tmp_dir, result_dir):
+def is_above_family_id(fam_name, above_family_id):
+    fam_id = fam_name.replace("mgnifam", "")
+    fam_id = float(fam_id)
+
+    if fam_id > above_family_id:
+        return True
+    else:
+        return False
+
+def query_sequence_explorer_protein(cursor, edge_list_file, above_family_id, tmp_dir, result_dir):
     with open(edge_list_file, 'r') as file:
         previous_col1 = None
         query_items = []
         for line in file:
             col1, col2 = line.strip().split('\t')
-            if col1 != previous_col1:
-                if query_items:  # Execute previous query if items exist
-                    execute_query(cursor, query_items, tmp_dir, result_dir)
-                query_items = []
-                previous_col1 = col1
-            parsed_col2 = parse_col2(col2)
-            query_items.append((col1, parsed_col2))
+            if (is_above_family_id(col1, above_family_id)):
+                if col1 != previous_col1:
+                    if query_items:  # Execute previous query if items exist
+                        execute_query(cursor, query_items, tmp_dir, result_dir)
+                    query_items = []
+                    previous_col1 = col1
+                parsed_col2 = parse_col2(col2)
+                query_items.append((col1, parsed_col2))
 
         # Execute the last query
         if query_items:
@@ -70,9 +80,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Query a PostgreSQL database and process an edge list file.")
     parser.add_argument("config_file", help="Path to the configuration file for the database secrets")
     parser.add_argument("edge_list_file", help="Path to the edge list file with two columns")
+    parser.add_argument("above_family_id", help="Threshold for family id, to not recalculate same ones")
     parser.add_argument("tmp_dir", help="Path to the tmp directory")
     parser.add_argument("result_dir", help="Path to the result directory")
-    # python3 bin/get_biome_distribution.py bin/db_config.ini data/families/updated_refined_families.tsv tmp/ data/biome_sunburst/tmp/
+    # python3 bin/get_biome_distribution.py bin/db_config.ini data/families/updated_refined_families.tsv 52 tmp/ data/biome_sunburst/tmp/
     args = parser.parse_args()
 
     os.makedirs(args.tmp_dir, exist_ok=True)
@@ -82,7 +93,7 @@ if __name__ == "__main__":
     conn = psycopg2.connect(**db_params)
     cursor = conn.cursor()
 
-    query_sequence_explorer_protein(cursor, args.edge_list_file, args.tmp_dir, args.result_dir)
+    query_sequence_explorer_protein(cursor, args.edge_list_file, args.above_family_id, args.tmp_dir, args.result_dir)
 
     cursor.close()
     conn.close()
--- bin/get_pfams.py
+++ bin/get_pfams.py
@@ -38,19 +38,29 @@ def execute_query(cursor, query_items, result_dir):
                 mgyp = row[0]
                 file.write(f"{mgyp}\t{metadata['p']}\n")
 
-def query_sequence_explorer_protein(cursor, edge_list_file, result_dir):
+def is_above_family_id(fam_name, above_family_id):
+    fam_id = fam_name.replace("mgnifam", "")
+    fam_id = float(fam_id)
+
+    if fam_id > above_family_id:
+        return True
+    else:
+        return False
+
+def query_sequence_explorer_protein(cursor, edge_list_file, above_family_id, result_dir):
     with open(edge_list_file, 'r') as file:
         previous_col1 = None
         query_items = []
         for line in file:
             col1, col2 = line.strip().split('\t')
-            if col1 != previous_col1:
-                if query_items:  # Execute previous query if items exist
-                    execute_query(cursor, query_items, result_dir)
-                query_items = []
-                previous_col1 = col1
-            parsed_col2 = extract_mgyp(col2)
-            query_items.append((col1, parsed_col2))
+            if (is_above_family_id(col1, above_family_id)):
+                if col1 != previous_col1:
+                    if query_items:  # Execute previous query if items exist
+                        execute_query(cursor, query_items, result_dir)
+                    query_items = []
+                    previous_col1 = col1
+                parsed_col2 = extract_mgyp(col2)
+                query_items.append((col1, parsed_col2))
 
         # Execute the last query
         if query_items:
@@ -60,8 +70,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Query a PostgreSQL database and process an edge list file.")
     parser.add_argument("config_file", help="Path to the configuration file for the database secrets")
     parser.add_argument("edge_list_file", help="Path to the edge list file with two columns")
+    parser.add_argument("above_family_id", help="Threshold for family id, to not recalculate same ones")
     parser.add_argument("result_dir", help="Path to the result directory")
-    # python3 bin/get_pfams.py bin/db_config.ini data/families/updated_refined_families.tsv data/pfams/tmp
+    # python3 bin/get_pfams.py bin/db_config.ini data/families/updated_refined_families.tsv 52 data/pfams/tmp
     args = parser.parse_args()
 
     os.makedirs(args.result_dir, exist_ok=True)
@@ -70,7 +81,7 @@ if __name__ == "__main__":
     conn = psycopg2.connect(**db_params)
     cursor = conn.cursor()
 
-    query_sequence_explorer_protein(cursor, args.edge_list_file, args.result_dir)
+    query_sequence_explorer_protein(cursor, args.edge_list_file, args.above_family_id, args.result_dir)
 
     cursor.close()
     conn.close()
