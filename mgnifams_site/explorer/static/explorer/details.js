ee36ed451d0470c87782082ff90744fda523acac
vagkaratzas <vagelaros.gee@gmail.com>
2024-04-17 13:17:24 +0100

    domains and datatables move to js file

--- mgnifams_site/explorer/static/explorer/details.js
+++ mgnifams_site/explorer/static/explorer/details.js
@@ -1,16 +1,3 @@
-const translate_to_hmm_pos = (sequence, position) => {
-    let hmm_pos = 0;
-
-    for (let i = 0; i < sequence.length; i++) {
-        if (sequence[i] === 'x') {
-            hmm_pos = hmm_pos + 1;
-            if (i >= position) break;
-        }
-    }
-
-    return hmm_pos;
-};
-
 const loadFamilyData = () => {
     document.getElementById('show-members-btn').addEventListener('click', function() {
         let div = document.getElementById('family-members-div');
@@ -80,10 +67,89 @@ const loadMSAData = () => {
     m.render();
 };
 
+const renderArchitecture = (jsonData) => {
+    const max_shown_length = 30;
+    const architecturesContainer = document.getElementById('architecturesContainer');
+
+    const showTooltip = (event, text, max_shown_length) => {
+        if (text.length > max_shown_length) {
+            const tooltip = document.getElementById('tooltip');
+            tooltip.textContent = text;
+            tooltip.style.display = 'block';
+            tooltip.style.left = event.pageX - window.innerWidth/5 + 'px';
+            tooltip.style.top = event.pageY  + 'px';
+        }
+    };
+
+    const hideTooltip = () => {
+        const tooltip = document.getElementById('tooltip');
+        tooltip.style.display = 'none';
+    };
+    
+    // Loop through each architecture container
+    jsonData.architecture_containers.slice(0, 10).forEach(container => {
+        const containerDiv = document.createElement('div');
+        containerDiv.classList.add('architecture-div');
+        const architectureTextPara = document.createElement('span');
+        architectureTextPara.textContent = `${container.architecture_text}`;
+        architectureTextPara.classList.add('descr-span');
+        containerDiv.appendChild(architectureTextPara);
+
+        // Loop through each domain in the container
+        container.domains.forEach(domain => {
+            const domainSpan = document.createElement('span');
+            const domainLink = document.createElement('a');
+            domainLink.textContent = domain.name.length > max_shown_length ? domain.name.substring(0, (max_shown_length - 3)) + "..." : domain.name;
+            domainLink.href = domain.link;
+            domainLink.setAttribute('target', '_blank');
+            domainLink.classList.add('domain-link');
+            domainSpan.classList.add('domain-span');
+            domainSpan.style.backgroundColor = domain.color;
+            domainSpan.style.color = domain.font_color;
+            domainSpan.appendChild(domainLink);
+            domainSpan.addEventListener('mouseover', function(event) {
+                showTooltip(event, domain.name, max_shown_length);
+            });
+            domainSpan.addEventListener('mouseout', hideTooltip);
+            containerDiv.appendChild(domainSpan);
+        });
+
+        architecturesContainer.appendChild(containerDiv);
+    });
+};
+
+const loadDomainData = () => {
+    let domainDataURL = document.getElementById('domain-data').dataset.url;
+    fetch(domainDataURL)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            renderArchitecture(data);
+        })
+        .catch(error => {
+            console.error('Error fetching domain architecture JSON:', error);
+        });
+};
+
+const loadDatatables = () => {
+    if ($('#pfams-table:contains("No Pfam hits found")').length === 0) {
+        $('#pfams-table').DataTable();
+    }
+    if ($('#structural-annotations-table:contains("No structural annotations found")').length === 0) {
+        $('#structural-annotations-table').DataTable();
+    }
+};
+
 $(document).ready(function () {
 
     loadFamilyData();
     loadBiomeData();
     loadMSAData();
-    
+    loadDomainData();
+    loadDatatables();
+
 })
