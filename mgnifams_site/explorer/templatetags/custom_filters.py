6b09be210d7b94a57a1ec1a6ec607f2b85cb94d8
vagkaratzas <vagelaros.gee@gmail.com>
2025-09-12 11:51:08 +0100

    switched from GET request to details/pk

--- mgnifams_site/explorer/templatetags/custom_filters.py
+++ mgnifams_site/explorer/templatetags/custom_filters.py
@@ -3,3 +3,16 @@
 from django import template
 
 register = template.Library()
+
+@register.filter
+def format_mgnifam_name(raw_name):
+    print("raw_name")
+    print(raw_name)
+    """
+    Formats the mgnifam name by appending zeros in front to make it 10 characters,
+    and then adds 'MGYF' as a prefix.
+    """
+    if raw_name is None:
+        return ""
+    formatted_name = str(raw_name).zfill(10)
+    return "MGYF" + formatted_name
--- mgnifams_site/explorer/urls.py
+++ mgnifams_site/explorer/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('mgnifams_list/', views.mgnifams_list, name='mgnifams_list'),
-    path('details/', views.details, name='details'),
+    path('details/<str:pk>/', views.details, name='details'),
     path('serve_blob/<int:pk>/<str:column_name>/', views.serve_blob_as_file, name='serve_blob_as_file'),
     # path('submit_hmmsearch/<int:mgyf_id>/', views.submit_hmmsearch, name='submit_hmmsearch'),
 ]
